ed7f95b646563604f8537474fe2a0f933eca30a8
REXgamer13 <gelvezbotiajuansebastian@gmail.com>
2026-03-23 14:34:00 -0500

    Agregar pruebas unitarias para el servicio de carrito y estrategias de descuento Paso 3 — Pruebas con JUnit 5

---
+++ src/test/java/com/universidad/pedidos/PedidosComportamiento2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universidad.pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidosComportamiento2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
